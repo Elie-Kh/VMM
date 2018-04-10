378d4d0de62479a4095fd04a4b20910ff7efb69e
Elie Kheirallah <kheirallahelie@gmail.com>
2018-04-10 15:35:53 -0400

    removed <<<<<head

--- VMM/Main.cpp
+++ VMM/Main.cpp
@@ -37,7 +37,7 @@ void updateTLB(int page, int frame, int index, vector<int> pageTLB, vector<int> 
 }
 
 int main() {
-<<<<<<< HEAD
+
 
 	FILE * backingStore;
 	char * buffer;
@@ -135,92 +135,5 @@ int main() {
 	system("pause");
 	return 0;
 
-=======
-    FILE * backingStore;
-    char * buffer;
-    size_t result;
-    long fileSize;
-    ifstream addresses("addresses.txt");
-    string readz;
-    int  entries = 0;
-    vector<int> addressEntries;
-    vector<int> pagesTLB (16, -1);   //TLB size 16 of value -1
-    vector<int> framesTLB (16, -1);  //TLB size 16
-    int pageTable [256];   //page table size 256 of value -1//////////////////////////////////
-    //vector<int> framesPTable (256, -1);   //page table size 256
-    int pageNum = 0;
-    int offsett = 0;
-    int frame = 0;
-    int pointerTLB = 0;
 
-    int tempo = 0;
-    backingStore = fopen("BACKING_STORE.bin", "rb");
-
-    if (backingStore == NULL) {
-        fputs("File error", stderr); exit(1);
-    }
-
-    fseek(backingStore, 0, SEEK_END);
-    fileSize = ftell(backingStore);
-    rewind(backingStore);
-
-    while (!addresses.eof()) {
-        getline(addresses, readz,'\n');
-        addressEntries.push_back(stoi(readz, nullptr, entries));
-
-        tempo++;
-    }
-
-    buffer = (char*)malloc(sizeof(char)*fileSize);
-    if(buffer == NULL) { fputs("File error", stderr); exit(2); }
-    result = fread(buffer, 1, fileSize, backingStore);
-    if (result != fileSize) {
-        fputs("Reading error", stderr); exit(3);
-    }
-
-    int i;
-    int index;
-    while(i < addressEntries.size() ){
-        pageNum = pageNumber(addressEntries[i]);
-        offsett = offset(addressEntries[i]);
-        index = searchTLB(pageNum,pagesTLB);
-        if(index == -1){    //TLB-miss
-            if(pageTable[pageNum] == -1){ //page Fault
-                /////////////////////////////////////////////
-                /////read backing_store.bin
-                /////store in available page frame in main memory
-                ///store frame number into frame
-                //frame =
-                /////page table tlb update
-                //pageTable[index] =
-                updateTLB(pageNum,frame,16,pagesTLB, framesTLB);
-
-            }
-            else{
-                frame = pageTable[index];
-            }
-            if(index == -1){    //page fault
-                /////////////////////////////////////////////////////////////////////////////////
-
-            }
-            else {  //page found into Page Table
-                frame = frameTable[index];
-                updateTLB(pageNum,frame,index,pagesPTable,framesPTable);    //update Page Table
-                updateTLB(pageNum,frame,16,pagesTLB, framesTLB);    //update TLB
-
-                ////////////////////////////////frame, offset, need to fetch from physical memory
-            }
-
-        }
-
-
-
-        i++;
-    }
-
-
-
-    system("pause");
-    return 0;
->>>>>>> 8c2dbf003bba26afac7dfac66785d54589629bc9
 }
