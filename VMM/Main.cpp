56f91913510c24b6d528596910bc60b78dde450e
Elie Kheirallah <kheirallahelie@gmail.com>
2018-04-09 23:39:47 -0400

    reading from address.txt done

--- VMM/Main.cpp
+++ VMM/Main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<stdio.h>
 #include<vector>
+#include<string>
+#include<fstream>
 using namespace std;
 
 int main() {
@@ -8,24 +10,41 @@ int main() {
 	char * buffer;
 	size_t result;
 	long fileSize;
+	ifstream addresses("addresses.txt");
+	string readz;
+	int  entries = 0;
+	vector<int> entriez;
+	int tempo = 0;
 	backingStore = fopen("BACKING_STORE.bin", "rb");
 	//vector<char> inputs(1000);
 	if (backingStore == NULL) {
 		fputs("File error", stderr); exit(1);
 	}
-
+	
 	fseek(backingStore, 0, SEEK_END);
 	fileSize = ftell(backingStore);
 	rewind(backingStore);
+	//entriez.push_back(fileSize);
+	while (!addresses.eof()) {
+		getline(addresses, readz,'\n');
+		entriez.push_back(stoi(readz, nullptr, entries));
+		//entriez[tempo] = stoi(readz, nullptr, entries);
+		
+		
+		tempo++;
+	}
+	for (int i = 0; i < entriez.size(); i++) {
+		cout << entriez[i] << "\n";
+	}
 	buffer = (char*)malloc(sizeof(char)*fileSize);
 	if(buffer == NULL) { fputs("File error", stderr); exit(2); }
 	result = fread(buffer, 1, fileSize, backingStore);
 	if (result != fileSize) {
 		fputs("Reading error", stderr); exit(3);
 	}
-	for (int i = 0; i < fileSize; i++) {
-		cout << (int)buffer[i];
-	}
+	//for (int i = 0; i < fileSize; i++) {
+	//	cout << (int)buffer[i];
+	//}
 	system("pause");
 	return 0;
 }
